dfb74111c265c689628109b723dc23d8387f2b90
Heptan <heptan716@gmail.com>
2021-05-04 02:02:09 +0200

    Add quality selector 0.1
    Started experimenting with quality selector feature and did some cleanup too.
    Now, we have a (asc) sorted list of available resoluions of the queried video. Woah! ^_^

--- PyTube_downloader.py
+++ PyTube_downloader.py
@@ -1,52 +1,59 @@
-import PySimpleGUI as sg
-from pytube import YouTube
+import PySimpleGUI as gui
+from pytube import YouTube,Stream
 from datetime import date
 import os
 import time
 import logging
 import subprocess
 import sys
+import traceback
 
 today = date.today() #date for new folder
 d1 = today.strftime("%Y_%m_%d")
 in_res=''
 
+gui.theme('DarkTeal2')
+
 
 layout = [
-			[sg.Text("Video URL")],
-			[sg.Input(),sg.Button('check')],
-			[sg.Text("Choose resolution")],
-			[sg.Button('H(1080p)')
-			,sg.Button('M(720p)')
-			,sg.Button('L(480p)')],
-			[sg.Checkbox('open dir', default=True, key='-DIR-'), sg.Checkbox('Open vid', default=False, key='-PLAY-')],
-			[sg.Text(size=(40,1), key='-OUTPUT-')],
-			[sg.Button('Ok'), sg.Button('Quit')],
-			[sg.Text(size=(80,5), key='-DEBUG-')],
+			[gui.Text("Video URL"), gui.Input(key='-URL-'), gui.Button('Fetch')],
+			[gui.Text("Choose resolution")],
+
+			[gui.Checkbox('open dir',default=True,key='-DIR-'), gui.Checkbox('Open vid',default=False,key='-PLAY-')],
+			[gui.Text(size=(40,1), key='-OUTPUT-')],
+			[gui.Button('Ok'), gui.Button('Quit')],
+			[gui.Multiline(size=(80,5),key='-DEBUG-', auto_refresh=True)],
 			]
 
-window = sg.Window('Youtube downloader', layout)
-event, values = window.read()
+window = gui.Window('Youtube downloader', layout)
 
 
 debugos = ''
 
-while event != 'Quit' or event != sg.WINDOW_CLOSED:
+while True:
 	event, values = window.read()
+	if event in ('Quit', gui.WINDOW_CLOSED):
+		break
 	
 
-	if event=='H(1080p)':
-		in_res='1080p'
-	elif event=='M(720p)':
-		in_res='720p'
-	elif event=='L(480p)':
-		in_res='480p'
-
-	if event=='check':
+	if event=='Fetch':
+		window['-DEBUG-'].update(values);
+		
 		
 		try:
-			vid = YouTube(values[0]) 
+			vid = YouTube(values['-URL-']) 
+			res_set = set()
+			dbg = ''
+			for stream in vid.streams.filter(only_video='true'):
+				dbg += 'Adding ' + stream.resolution + '\n'
+				window['-DEBUG-'].update(dbg);
+				res_set.add(stream.resolution)
+			res_list = []
+			res_list.extend(res_set)
+			res_list.sort(key=lambda r: int(r[:-1]))
+			window['-DEBUG-'].update(res_list);
 		except:
+			window['-DEBUG-'].update(sys.exc_info());
 			window['-OUTPUT-'].update('Invalid URL!', text_color='red')
 		else:
 			if in_res!='':
@@ -56,7 +63,7 @@ while event != 'Quit' or event != sg.WINDOW_CLOSED:
 
 	
 	if event == 'Ok':
-		vid = YouTube(values[0])
+		vid = YouTube(values['-URL-'])
 		if in_res=='':
 			window['-OUTPUT-'].update('"' + vid.title+ '"' +' Please select a resolution!', text_color='yellow')
 		else:
@@ -81,7 +88,7 @@ while event != 'Quit' or event != sg.WINDOW_CLOSED:
 				os.startfile(r"D:\youtubedl"'\\' +d1+ r"_vid"'\\' + new + '.mp4' )
 
 		
-	if event == sg.WINDOW_CLOSED or event == 'Quit':
+	if event == gui.WINDOW_CLOSED or event == 'Quit':
 		break
 
 window.close()
